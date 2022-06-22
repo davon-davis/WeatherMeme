8c3a7470a62106893e1dfbf725cef79755fa3634
davon <davon.l.davis.mil@army.mil>
2022-06-22 15:25:56 -0500

    second commit

---
+++ src/main/java/com/example/weathermeme/WeatherMemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.weathermeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/weathermeme/WeatherMemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weathermeme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
