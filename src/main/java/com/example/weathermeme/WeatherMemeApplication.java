3ea5f76fd70d394b79159270cdf7fa21f73a9490
davon <davon.l.davis.mil@army.mil>
2022-06-22 15:45:13 -0500

    made some structual changes

--- src/main/java/com/example/weathermeme/WeatherMemeApplication.java
+++ src/main/java/com/example/weathermeme/WeatherMemeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WeatherMemeApplication {
+public class WeathermemeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeatherMemeApplication.class, args);
+		SpringApplication.run(WeathermemeApplication.class, args);
 	}
 
 }
