e4e2f7f813de34e2e60eef28e4b749c4fdc9d914
Davon Davis <davon.l.davis.mil@swf.army.mil>
2023-04-03 10:37:02 -0500

    setup repo on new machine

--- src/main/java/com/example/weathermeme/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.weathermeme.repositories;
-
-import com.example.weathermeme.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users, Long> {
-}
